d24ae821b6dd09a2b3b50febe88c68ce57eb96fb
Vivek Vamsi Inala <vivekvamsiinala@gmail.com>
2023-07-27 15:52:27 -0600

    add gomock Tests starter

---
+++ internal/controller/spireserver_controller_mock_test.go
@@ -0,0 +1,80 @@
+package controller
+
+import (
+	"context"
+	"testing"
+
+	// "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	spirev1 "github.com/glcp/spire-k8s-operator/api/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type MockClient struct {
+	client.Client
+	CreateFn func(context.Context, client.Object, ...client.CreateOption) error
+}
+
+func (m *MockClient) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+	if m.CreateFn != nil {
+		return m.CreateFn(ctx, obj, opts...)
+	}
+	return nil
+}
+
+func TestSpireserverController(t *testing.T) {
+	// Create the objects needed for the test
+	reconciler := &SpireServerReconciler{
+		Client: &MockClient{
+			CreateFn: func(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+				// Handle the create logic in the mock client
+				return nil
+			},
+		},
+		Scheme: scheme.Scheme,
+	}
+
+	spireserver := &spirev1.SpireServer{}
+	spireServiceNamespace := "test-namespace"
+	spireServiceAccount := reconciler.createServiceAccount(spireServiceNamespace)
+	bundle := reconciler.spireBundleDeployment(spireServiceNamespace)
+	roles := reconciler.spireRoleDeployment(spireServiceNamespace)
+	roleBinding := reconciler.spireRoleBindingDeployment(spireServiceNamespace)
+	clusterRoles := reconciler.spireClusterRoleDeployment(spireServiceNamespace)
+	clusterRoleBinding := reconciler.spireClusterRoleBindingDeployment(spireServiceNamespace)
+	serverConfigMap := reconciler.spireConfigMapDeployment(spireserver, spireServiceNamespace)
+	spireStatefulSet := reconciler.spireStatefulSetDeployment(2, spireServiceNamespace)
+	spireService := reconciler.spireServiceDeployment(8081, spireServiceNamespace)
+
+	// Call the method you want to test
+	// Assert the expected behavior
+
+	if spireServiceAccount.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, spireServiceAccount.Namespace)
+	}
+	if bundle.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, bundle.Namespace)
+	}
+	if roles.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, roles.Namespace)
+	}
+	if roleBinding.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, roleBinding.Namespace)
+	}
+	if clusterRoles.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, clusterRoles.Namespace)
+	}
+	if clusterRoleBinding.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, clusterRoleBinding.Namespace)
+	}
+	if serverConfigMap.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, serverConfigMap.Namespace)
+	}
+	if spireStatefulSet.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, spireStatefulSet.Namespace)
+	}
+	if spireService.Namespace != spireServiceNamespace {
+		t.Errorf("Expected namespace %s, got %s", spireServiceNamespace, spireService.Namespace)
+	}
+}
